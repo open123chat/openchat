1196938b2199ed8e10226190ac2276413b11c781
kimjeonghyun <rlawjdgus051@naver.com>
2022-02-19 16:41:55 +0900

    back maven 수정

---
+++ back/src/main/java/com/example/openchat/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.example.openchat.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public ResponseEntity main() {
+        return new ResponseEntity<>(1, HttpStatus.OK);
+    }
+}
